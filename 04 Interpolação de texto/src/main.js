5f7e0168708bdabc5c6e0cd18379661bcef4dca7
GersonESS <gebhsantos@gmail.com>
2025-02-13 23:57:11 -0300

    a

--- 04 Interpolação de texto/src/main.js
+++ 04 Interpolação de texto/src/main.js
@@ -1,6 +1,11 @@
-import './assets/main.css'
+// Modulo createApp
 
 import { createApp } from 'vue'
+
+// Importando arquivo App.vue
+
 import App from './App.vue'
+
+//Render 
 
 createApp(App).mount('#app')
