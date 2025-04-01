047c0dbcb8bbac863ff68c540e98adeffdc74643
GersonESS <gebhsantos@gmail.com>
2025-04-01 16:55:50 -0300

    a

---
+++ 04 Interpolação de texto/vue-project/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+// Variáveis
+const nome = 'Fulano';
+const idade = 30;
+</script>
+
+<!-- html -->
+
+<template>
+  <h1>{{ 'Olá ' + nome + ' voce tem ' + idade + ' anos '}}</h1>
+  <p>{{ idade }}</p>
+</template>
