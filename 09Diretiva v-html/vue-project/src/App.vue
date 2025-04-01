d8d963796b00890607cb62bcf7649647e9eaa2cb
GersonESS <gebhsantos@gmail.com>
2025-04-01 17:45:23 -0300

    a

--- 09Diretiva v-html/vue-project/src/App.vue
+++ 09Diretiva v-html/vue-project/src/App.vue
@@ -3,7 +3,7 @@
 // importando o componente
 // import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
 // import Exemplo02 from './componentes/Exemplo02Interpolacao.vue';
-import Exemplo04 from './componentes/Exemplo04Interpolacao.vue';
+import Exemplo03 from './componentes/Exemplo03Interpolacao.vue';
 
 </script>
 
@@ -11,5 +11,5 @@ import Exemplo04 from './componentes/Exemplo04Interpolacao.vue';
 <!-- html -->
 
 <template>
-  <Exemplo04></Exemplo04>
+  <Exemplo03></Exemplo03>
 </template>
