98a333dfb641f1290f158c1f4966c6f985fc3064
GersonESS <gebhsantos@gmail.com>
2025-04-01 17:14:08 -0300

    a

---
+++ 05Trabalhando com componentes/vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+// Variáveis
+const nome = 'Fulano';
+const idade = 30;
+let numero1 = 10;
+let numero2 = 5;
+</script>
+
+<!-- html -->
+
+<template>
+  <h1>{{ 'Olá ' + nome + ' voce tem ' + idade + ' anos. '}}</h1>
+  <p>{{ idade }}</p>
+  <p>{{ numero1 + numero2 }}</p>
+  <p>{{ numero1 - numero2 }}</p>
+  <p>{{ numero1 * numero2 }}</p>
+  <p>{{ numero1 / numero2 }}</p>
+</template>
