995815ff2fc0a8e63d2a346b0d839725b6c5347f
GersonESS <gebhsantos@gmail.com>
2025-04-02 19:38:53 -0300

    f

--- 10Diretiva v-model/vue-project/src/App.vue
+++ 10Diretiva v-model/vue-project/src/App.vue
@@ -3,13 +3,13 @@
 // importando o componente
 // import Exemplo01 from './componentes/Exemplo01Interpolacao.vue';
 // import Exemplo02 from './componentes/Exemplo02Interpolacao.vue';
-import Exemplo04 from './componentes/Exemplo04Interpolacao.vue';
-
+// 
+import Exemplo05 from './componentes/Exemplo05Model.vue';
 </script>
 
 
 <!-- html -->
 
 <template>
-  <Exemplo04></Exemplo04>
+  <Exemplo05></Exemplo05>
 </template>
