aee2104b6d958b194e2ce0426dfbc1257fbc0c2d
GersonESS <gebhsantos@gmail.com>
2025-04-02 11:21:17 -0300

    a

---
+++ bk/src/componentes/Exemplo05Model.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+let texto = '';
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Exemplo 05 - Model</h1>
+    <input type="text" v-model="texto" />
+    <p>{{ texto }}</p>
+  </div>
+</template>
