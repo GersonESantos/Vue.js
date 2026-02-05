7dd9f7a4c125039dc567e23df2fbe396b52891e5
GersonESS <gebhsantos@gmail.com>
2026-02-05 05:53:35 -0300

    a

---
+++ Masterclass Vue.JS 2024/02Limpo/src/App.vue
@@ -0,0 +1,13 @@
+
+
+<script setup>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+</script>
+
+<template>
+  <PrimeiroComponente />
+</template>
+
+<style scoped>
+
+</style>
