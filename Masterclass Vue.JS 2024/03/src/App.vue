0062690df5b1c730611dcc0aa41c6526e036f1f0
GersonESS <gebhsantos@gmail.com>
2026-02-05 06:13:24 -0300

    z

---
+++ Masterclass Vue.JS 2024/03/src/App.vue
@@ -0,0 +1,18 @@
+
+
+<script setup>
+import ComDados from "./components/ComDados.vue";
+import PrimeiroComponente from "./components/PrimeiroComponente.vue"
+</script>
+
+<template>
+  <header>
+    vue
+  </header>
+  <ComDados />
+  <PrimeiroComponente />
+</template>
+
+<style scoped>
+
+</style>
