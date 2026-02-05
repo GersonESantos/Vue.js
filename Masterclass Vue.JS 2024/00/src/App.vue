74d5e29fdd25a5fb985378c9b3be5c065596a4a4
GersonESS <gebhsantos@gmail.com>
2026-02-05 09:02:54 -0300

    ]

--- Masterclass Vue.JS 2024/00/src/App.vue
+++ Masterclass Vue.JS 2024/00/src/App.vue
@@ -2,15 +2,16 @@
 
 <script setup>
 import ComDados from "./components/ComDados.vue";
+import ComDadosReativosPrimitivos from "./components/ComDadosReativosPrimitivos.vue";
 import PrimeiroComponente from "./components/PrimeiroComponente.vue"
 </script>
 
 <template>
-  <header>
-    vue
-  </header>
+  <header>vue</header>
+  <ComDadosReativosPrimitivos />
   <ComDados />
   <PrimeiroComponente />
+  
 </template>
 
 <style scoped>
